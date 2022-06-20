80de421c64a92c544845ea15a4baa08e41555809
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2022-06-20 08:26:18 -0400

    booger

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function inView(element)
+{
+    var visible = false;
+    var rect = element.getBoundingClientRect();
+    var top = rect.top;
+    var bottom = rect.bottom;
+
+    if (top >= 0 && bottom <= window.innerWidth) visible = true;
+
+    return visible;
+}
+
+
+document.addEventListener("scroll", () =>
+{
+    var nav = document.getElementsByTagName("nav")[0];
+    var elem = document.getElementById("projects");
+    if (inView(elem)) 
+    {
+        if (nav.classList.contains("navbar-expand-sm")) nav.classList.remove("navbar-expand-sm");
+    }
+
+    else nav.classList.add("navbar-expand-sm");
+});
